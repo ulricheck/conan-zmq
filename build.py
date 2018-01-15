5a2c2663c5a17ae57d267b95e8756ee4ddf170f2
Ulrich Eck <ueck@net-labs.de>
2018-01-15 16:29:50 +0100

    update to avoid x86 cross-compilation on windows and only provide shared libs through CI for now

--- build.py
+++ build.py
@@ -3,4 +3,9 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="zmq:shared", pure_c=True)
+    builder.builds = [
+        [settings, options, env_vars, build_requires]
+        for settings, options, env_vars, build_requires in builder.builds
+        if (not (os_info.is_linux and settings["arch"] == "x86")) and options['zmq:shared'] == True 
+    ]
     builder.run()
