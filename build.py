08bcc3c1ab421767a7ad9b2d6a80344ff61ceef1
Ulrich Eck <ulrich.eck@tum.de>
2018-05-28 21:43:57 +0200

    updated build.py

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="zmq:shared", pure_c=True)
     builder.builds = [
         [settings, options, env_vars, build_requires]
-        for settings, options, env_vars, build_requires in builder.builds
+        for settings, options, env_vars, build_requires in builder.items
         if (not (os_info.is_linux and settings["arch"] == "x86")) and options['zmq:shared'] == True 
     ]
     builder.run()
