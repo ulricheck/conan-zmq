399d385c92fc7b1cd1339377f29779aea6e80fd9
Ulrich Eck <ulrich.eck@tum.de>
2018-05-28 22:41:02 +0200

    fixed build.py

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="zmq:shared", pure_c=True)
     builder.builds = [
-        [settings, options, env_vars, build_requires]
-        for settings, options, env_vars, build_requires in builder.items
+        [settings, options, env_vars, build_requires, reference]
+        for settings, options, env_vars, build_requires, reference in builder.items
         if (not (os_info.is_linux and settings["arch"] == "x86")) and options['zmq:shared'] == True 
     ]
     builder.run()
